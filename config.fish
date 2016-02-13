3a9afb8ec314f4b755bdbbe787246189420d6d08
Dennis Hübner <dennis.huebner89@gmail.com>
2016-02-13 18:06:38 +0100

    wtf

--- config.fish
+++ config.fish
@@ -50,24 +50,24 @@ function export
   set -gx $arr[1] $arr[2]
 end
 
-set PATH $HOME/.jenv/shims $PATH
-command jenv rehash 2>/dev/null
-function jenv
-  set cmd $argv[1]
-  set arg ""
-  if test (count $argv) -gt 1
-    # Great... fish first array index is ... 1 !
-    set arg $argv[2..-1]
-  end
-
-  switch "$cmd"
-    case enable-plugin rehash shell shell-options
-        set script (jenv "sh-$cmd" "$arg")
-        eval $script
-    case '*'
-        command jenv $cmd $arg
-    end
-end
+#set PATH $HOME/.jenv/shims $PATH
+#command jenv rehash 2>/dev/null
+#function jenv
+#  set cmd $argv[1]
+#  set arg ""
+#  if test (count $argv) -gt 1
+#    # Great... fish first array index is ... 1 !
+#    set arg $argv[2..-1]
+#  end
+#
+#  switch "$cmd"
+#    case enable-plugin rehash shell shell-options
+#        set script (jenv "sh-$cmd" "$arg")
+#        eval $script
+#    case '*'
+#        command jenv $cmd $arg
+#    end
+#end
 
 set PATH /usr/local/texlive/2015/bin/x86_64-darwin $PATH
 set PATH /Users/dennis/.local/bin $PATH
